4e8ebdbd5d98abc6ffa723c851ad360a11b37e92
Starmaker2021 <2968722911@qq.com>
2023-02-06 16:35:19 +0000

    2-06

---
+++ src/twoSum_1.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                res[1] = i;
+                res[0] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
+
