1d1f5bfa2fcbbac7927ed53d157cf28e91ae0e3e
Starmaker2021 <2968722911@qq.com>
2023-02-16 11:50:15 +0000

    2-16

---
+++ src/CountNodes.java
@@ -0,0 +1,8 @@
+public class CountNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return countNodes(root.right) + countNodes(root.left) + 1;
+    }
+}
