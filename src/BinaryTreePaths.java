46534bbfa3cc6e9ca21d79068f84fa84ed61a21e
Starmaker2021 <2968722911@qq.com>
2023-02-17 10:24:38 +0000

    2-17

---
+++ src/BinaryTreePaths.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        List<Integer> paths = new ArrayList<>();
+        traversal(root, paths, res);
+        return res;
+    }
+
+    private void traversal(TreeNode root, List<Integer> paths, List<String> res) {
+        paths.add(root.val);
+        if (root.right != null && root.left != null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < paths.size() - 1; i++) {
+                sb.append(paths.get(i)).append("->");
+            }
+            sb.append(paths.get(paths.size()-1));
+            res.add(sb.toString());
+            return;
+        }
+        if (root.left != null) {
+            traversal(root.left, paths, res);
+            paths.remove(paths.size() - 1);
+        }
+        if (root.right != null) {
+            traversal(root.right, paths, res);
+            paths.remove(paths.size() - 1);
+        }
+
+    }
+}
---
+++ src/GetHeight.java
@@ -0,0 +1,17 @@
+
+public class GetHeight {
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = getHeight(root.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+
+        return Math.max(leftHeight,rightHeight)+1;
+
+    }
+}
