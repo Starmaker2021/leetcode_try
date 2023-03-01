805b294b8385cf6c476de20efcfdf6c6e3d31736
Starmaker2021 <2968722911@qq.com>
2023-03-01 12:04:32 +0000

    3-01

---
+++ src/Permute.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Permute {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+    public List<List<Integer>> permute(int[] nums){
+        if(nums.length==0){
+            return result;
+        }
+        used = new boolean[nums.length];
+        permuteHelper(nums);
+        return result;
+    }
+    private void permuteHelper(int[] nums){
+        if(path.size()==nums.length){
+            result.add(new ArrayList<>(paht));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if(used[i]){
+                continue;
+            }
+            used[i]=true;
+            path.add(nums[i]);
+            permuteHelper(nums);
+            path.removeLast();
+            used[i]=false;
+        }
+    }
+}
