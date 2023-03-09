26ab15d49fdf7c770ad00e414b2c2b0b8d2d752f
Starmaker2021 <2968722911@qq.com>
2023-03-09 10:26:48 +0000

    3-09

---
+++ src/MonotoneIncreasingDigits.java
@@ -0,0 +1,16 @@
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int N) {
+        String[] strings = (N + "").split("");
+        int flag = strings.length;
+        for (int i = strings.length - 1; i > 0; i--) {
+            if (Integer.parseInt(strings[i])<Integer.parseInt(strings[i-1])){
+                strings[i-1]=(Integer.parseInt(strings[i-1])-1)+"";
+                flag = i;
+            }
+            for (int j = flag; j < strings.length; j++) {
+                strings[i]="9";
+            }
+            return Integer.parseInt(String.join("",strings));
+        }
+    }
+}
